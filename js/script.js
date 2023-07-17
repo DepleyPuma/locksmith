ef541456eacc16b29b1c27f9b150a5be0f89466b
DepleyPuma <szymon.szarek1@wp.pl>
2023-07-17 11:40:36 +0200

    added nav scripts

--- js/script.js
+++ js/script.js
@@ -1,27 +1,15 @@
-const nav = document.querySelector('.nav');
+const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
 
 const handleNav = () => {
-	nav.classList.toggle("nav--active");
-
-	navBtnBars.classList.remove('black-bars-color');
+	nav.classList.toggle("show");
 
 	allNavItems.forEach((item) => {
 		item.addEventListener("click", () => {
-			nav.classList.remove("nav--active");
+			nav.classList.remove("show");
 		});
 	});
-
-	handleNavItemsAnimation();
 };
 
-const handleNavItemsAnimation = () => {
-	let delayTime = 0;
-
-	allNavItems.forEach((item) => {
-		item.classList.toggle("nav-items-animation");
-		item.style.animationDelay = "." + delayTime + "s";
-		delayTime++;
-	});
-};
+navBtn.addEventListener("click", handleNav);
